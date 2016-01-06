465d179d28964abbc6605570ef4ccefc7e1e4276
stahc1596 <stahc1596@HRH-IC0027346.wrdsb.ca>
2016-01-06 13:32:27 -0500

    Cameron S.

---
+++ src/finalsummative/Moving.java
@@ -0,0 +1,158 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package finalsummative;
+
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author stahc1596
+ */
+
+public class Moving extends JComponent implements KeyListener{
+
+    // Height and Width of our game
+    static final int WIDTH = 800;
+    static final int HEIGHT = 600;
+    
+    // sets the framerate and delay for our game
+    // you just need to select an approproate framerate
+    long desiredFPS = 60;
+    long desiredTime = (1000)/desiredFPS;
+    int x = 100;
+    int y = 150;
+    boolean w = false;
+    boolean s = false;
+    boolean a = false;
+    boolean d = false;
+
+    
+    // drawing of the game happens in here
+    // we use the Graphics object, g, to perform the drawing
+    // NOTE: This is already double buffered!(helps with framerate/speed)
+    @Override
+    public void paintComponent(Graphics g)
+    {
+        // always clear the screen first!
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+        
+        // GAME DRAWING GOES HERE
+        g.setColor(Color.GRAY);
+        g.fillRect(100, 100, 50, 50);
+        g.fillRect(100, 150, 50, 50);
+        g.setColor(Color.BLACK);
+        g.drawRect(100, 100, 50, 50);
+        g.drawRect(100, 150, 50, 50);
+        g.setColor(Color.BLUE);
+        g.fillOval(x, y, 50, 50);
+        
+        // GAME DRAWING ENDS HERE
+    }
+    
+    
+    // The main game loop
+    // In here is where all the logic for my game will go
+    public void run()
+    {
+        // Used to keep track of time used to draw and update the game
+        // This is used to limit the framerate later on
+        long startTime;
+        long deltaTime;
+        
+        // the main game loop section
+        // game will end if you set done = false;
+        boolean done = false; 
+        while(!done)
+        {
+            // determines when we started so we can keep a framerate
+            startTime = System.currentTimeMillis();
+            
+            // all your game rules and move is done in here
+            // GAME LOGIC STARTS HERE 
+            if (w){
+                y = y - 50;
+            }if (s){
+                y = y + 50;
+            }
+            
+
+            // GAME LOGIC ENDS HERE 
+            
+            // update the drawing (calls paintComponent)
+            repaint();
+            
+            
+            
+            // SLOWS DOWN THE GAME BASED ON THE FRAMERATE ABOVE
+            // USING SOME SIMPLE MATH
+            deltaTime = System.currentTimeMillis() - startTime;
+            if(deltaTime > desiredTime)
+            {
+                //took too much time, don't wait
+            }else
+            {
+                try
+                {
+                    Thread.sleep(desiredTime - deltaTime);
+                }catch(Exception e){};
+            }
+        }
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // creates a windows to show my game
+        JFrame frame = new JFrame("My Game");
+       
+        // creates an instance of my game
+        Moving game = new Moving();
+        // sets the size of my game
+        game.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        // adds the game to the window
+        frame.add(game);
+         
+        // sets some options and size of the window automatically
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        // shows the window to the user
+        frame.setVisible(true);
+        
+        // starts my game loop
+        game.run();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_W){
+            w = true;
+        }else if (e.getKeyCode() == KeyEvent.VK_S){
+            s = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_W){
+            w = false;
+        }else if (e.getKeyCode() == KeyEvent.VK_S){
+            s = false;
+        }
+    }
+}
